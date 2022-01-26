96aab8d195ec193c799496382f3049039f3efebc
djboesch <98228834+djboesch@users.noreply.github.com>
2022-01-25 18:13:31 -0600

    Update and rename chart_PAE&LDDT.py to chart_PAE.py

--- chart_PAE.py
+++ chart_PAE.py
@@ -5,20 +5,13 @@ import numpy as np
 with open(r'C:\Users\rest\of\path', 'rb') as f:
     data = pickle.load(f)
     print("sucessfully loaded")
-    
-plddt = data["plddt"]
-plt.figure(figsize=[8, 6])
-plt.subplot(1, 2, 1)
-plt.plot(plddt)
-plt.title('Predicted LDDT')
-plt.xlabel('Residue')
-plt.ylabel('pLDDT')
+   
 
 
 pae_output = (data['predicted_aligned_error'],
               data['max_predicted_aligned_error'])
 
-plt.subplot(1, 2, 2)
+plt.subplot(1, 1, 1)
 pae, max_pae = pae_output
 plt.imshow(pae, vmin=0., vmax=max_pae, cmap='Greens_r')
 plt.colorbar(fraction=0.046, pad=0.04)
